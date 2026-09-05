8da14335a55fe78c17f394c155741503a04286eb
stanis miafo <stanismiafo348@gmail.com>
2026-09-05 11:21:57 +0100

    premier apk

--- cyclia/lib/services/config.dart
+++ cyclia/lib/services/config.dart
@@ -1,13 +1,4 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
-
 class ApiConfig {
-  static String get baseUrl {
-    if (kIsWeb) {
-      return 'http://localhost:8000/api';
-    }
-    // For mobile platforms, use conditional import or default to localhost
-    // Android emulator uses 10.0.2.2 to reach host machine
-    // For now, use localhost as default (works for iOS simulator and desktop)
-    return 'http://10.0.2.2:8000/api';
-  }
+  // Lien du backend en ligne sur PythonAnywhere
+  static const String baseUrl = 'https://cyclia.pythonanywhere.com/api';
 }
